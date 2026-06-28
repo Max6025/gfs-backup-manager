a56617bf1ce25a19f2c7fa0577166a03203cdb86
Max6025 <69343783+Max6025@users.noreply.github.com>
2026-06-28 13:46:11 +0200

    Add: WebSocket Push + Testmodus

--- custom_components/gfs_backup_manager/const.py
+++ custom_components/gfs_backup_manager/const.py
@@ -3,12 +3,11 @@
 DOMAIN = "gfs_backup_manager"
 ADDON_SLUG = "b9d02817_gfs_backup"
 ADDON_NAME = "GFS Backup Manager"
-# Addon läuft auf dem HA-Host, Port 8099 ist nach außen freigegeben
 ADDON_API_PORT = 8099
+ADDON_WS_PORT = 8098
 
 SCAN_INTERVAL_SECONDS = 60
 
-# Backup-Typen
 BACKUP_TYPES = ["daily", "weekly", "monthly", "yearly"]
 
 BACKUP_TYPE_LABELS = {
@@ -24,14 +23,3 @@ BACKUP_PREFIXES = {
     "monthly": "HA-Monthly-",
     "yearly": "HA-Yearly-",
 }
-
-# stdin Befehle ans Addon
-CMD_TRIGGER_DAILY = "trigger_daily"
-CMD_TRIGGER_WEEKLY = "trigger_weekly"
-CMD_TRIGGER_MONTHLY = "trigger_monthly"
-CMD_TRIGGER_YEARLY = "trigger_yearly"
-CMD_DELETE_LAST_LOCAL = "delete_last_local"
-CMD_DELETE_LAST_NAS_DAILY = "delete_last_nas_daily"
-CMD_DELETE_LAST_NAS_WEEKLY = "delete_last_nas_weekly"
-CMD_DELETE_LAST_NAS_MONTHLY = "delete_last_nas_monthly"
-CMD_DELETE_LAST_NAS_YEARLY = "delete_last_nas_yearly"
